92131842877a1ba8a77dba6e6cc44388f09b955d
skbrown333 <skbrown333@gmail.com>
2023-07-25 14:07:01 -0700

    fix port

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import "dotenv/config";
 import app from "./src/app";
 
-app.listen(3000, () => console.log(`Listening on port ${3000}`));
+app.listen(process.env.PORT ?? 3000, () =>
+  console.log(`Listening on port ${process.env.PORT ?? 3000}`)
+);
