7de34e96ffd5233fcf48f99cceb61a9adf98a25e
skbrown333 <skbrown333@gmail.com>
2023-08-23 12:24:18 -0700

    parse boolen filters

--- src/middleware/QueryStrings.ts
+++ src/middleware/QueryStrings.ts
@@ -3,9 +3,11 @@ import qs from "qs";
 
 export const parseQueryStrings = (req, res, next) => {
   const query = req.query ?? {};
+  const parsedFilters = qs.parse(query.filters);
   query.filters = query.filters ?? {};
   query.options = query.options ?? {};
-  query.filters = parseObjectIds(qs.parse(query.filters));
+  query.filters = parseObjectIds(parsedFilters);
+  query.filters = parseBooleans(query.filters);
   req.query = query;
   next();
 };
@@ -13,6 +15,14 @@ export const parseQueryStrings = (req, res, next) => {
 export function parseObjectIds(query) {
   Object.keys(query).forEach(function (key) {
     if (ObjectId.isValid(query[key])) query[key] = new ObjectId(query[key]);
+  });
+  return query;
+}
+
+export function parseBooleans(query) {
+  Object.keys(query).forEach(function (key) {
+    if (query[key] === "false" || query[key] === "true")
+      query[key] = query[key] === true;
   });
   return query;
 }
