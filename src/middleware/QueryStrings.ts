dbe2dc504f5e288aace1bd2f2ec53a8c428c4521
Steffan <skbrown333@gmail.com>
2023-09-24 02:16:19 -0700

    print out req

--- src/middleware/QueryStrings.ts
+++ src/middleware/QueryStrings.ts
@@ -4,7 +4,7 @@ import qs from "qs";
 export const parseQueryStrings = (req, res, next) => {
   const query = req.query ?? {};
   const parsedFilters = qs.parse(query.filters);
-  console.log("cookies: ", req.headers.cookie);
+  console.log("cookies: ", req);
   query.filters = query.filters ?? {};
   query.options = query.options ?? {};
   query.filters = parseObjectIds(parsedFilters);
